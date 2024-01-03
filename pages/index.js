427c237ae462ce59b56fbea59d24fc490a4e681b
Allan Cruvinel <allan.cruvinel@gmail.com>
2024-01-03 01:52:33 +0000

    teste4

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Mtu loco ne </h1>
+    return <>
+    <h1>e ai que fazes?</h1>
+    <img src="https://media.tenor.com/X73EqPfwAfIAAAAM/minion-any-questions-question.gif" alt="Descrição do GIF" />
+    </> 
+     
+
 }
 
 export default Home;
