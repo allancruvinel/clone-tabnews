e65b67a25d242f705b7c6189fa2766b869dda72b
Allan Cruvinel <allan.cruvinel@gmail.com>
2024-01-16 15:33:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function Home() {
-  return <>
-    <h1>e ai que fazes?</h1>
-    <img src="https://media.tenor.com/X73EqPfwAfIAAAAM/minion-any-questions-question.gif" alt="Descrição do GIF" />
-  </>
+  return (
+    <>
+      <h1>e ai que fazes?</h1>
+      <img
+        src="https://media.tenor.com/X73EqPfwAfIAAAAM/minion-any-questions-question.gif"
+        alt="Descrição do GIF"
+      />
+    </>
+  );
 }
-
 export default Home;
