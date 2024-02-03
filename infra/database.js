85f583de41bb9f769cdae791b563ff3541291304
Allan Cruvinel <allan.cruvinel@gmail.com>
2024-02-03 01:16:20 +0000

    makes `database.js` `client.connec()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,19 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   try {
+    console.log("Credenciais do postgres ", {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+    });
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (ex) {
-    console.log(ex);
+    console.error(ex);
+    throw ex;
   } finally {
     await client.end();
   }
